276373e3dcc69e402b31b77022896e9b38d51a58
Peter Karman <karpet@peknet.com>
2022-06-08 08:40:31 -0500

    normalize AC ds2

--- bin/allegheny/d2-norm.pl
+++ bin/allegheny/d2-norm.pl
@@ -10,9 +10,14 @@ use QIC::Utils qw( parse_date_abbrev read_json trim );
 
 my $usage = "$0 afile bfile cfile";
 
-my %MAP = ();
+my $MAX_PERPS = 10;
 
 # load allegation mapping
+my $allegation_mapping = csv(
+    in      => "$FindBin::Bin/../../eval/allegheny-allegation-mapping.csv",
+    headers => "auto"
+);
+my %allegation_map = map { $_->{allegation} => $_ } @$allegation_mapping;
 
 my %allegation_codes = (
     "n/a"                                     => "",
@@ -27,49 +32,51 @@ my %allegation_codes = (
     "unknown or missing"                      => 9,
 );
 
+sub get_allegation_code {
+    my $abs       = shift            or return "";
+    my $orig_text = $abs->{ABS_TYPE} or return "";
+
+    # ambiguous original values
+    return 9 if $orig_text eq 'Causing';
+    return 9 if $orig_text eq 'Contributing To';
+    return 9 if $orig_text eq 'Exposing';
+
+    if ( !exists $allegation_map{$orig_text} ) {
+        die "Missing allegation map for '$orig_text'";
+    }
+    my $name = $allegation_map{$orig_text}->{ba}
+        || $allegation_map{$orig_text}->{njk};
+    if ( !$name ) {
+        die "No allegation map for '$orig_text'";
+    }
+    if ( !exists $allegation_codes{$name} ) {
+        die "No allegation code for '$name'";
+    }
+    return $allegation_codes{$name};
+}
+
+my %sex = (
+    ""        => "",
+    "male"    => 1,
+    "n/a"     => "",
+    "female"  => 2,
+    "unknown" => 9,
+);
+
 sub remap {
     my ( $rec, $field, $map ) = @_;
     if ( !exists $rec->{$field} ) {
         warn "field $field not in record: " . dump($rec);
         return;
     }
+    if ( !defined $rec->{$field} ) {
+        return $map->{""};
+    }
     if ( !exists $map->{ lc( $rec->{$field} ) } ) {
         warn "No mapping for $field => $rec->{$field} in map";
         return;
     }
     $rec->{$field} = $map->{ lc( $rec->{$field} ) };
-}
-
-my @dates = qw(
-    contact_date
-);
-
-my %warned = ();
-
-sub norm_rec {
-    my ( $file, $rec ) = @_;
-
-    my $normed = {};
-    for my $k ( keys %$rec ) {
-        next unless $k;
-        if ( !exists $MAP{$k} ) {
-            warn "$file: Key $k not in MAP" unless $warned{$file}->{$k}++;
-            $normed->{$k} = $rec->{$k};
-            next;
-        }
-        next if $MAP{$k} eq "";
-        $normed->{ $MAP{$k} } = $rec->{$k};
-    }
-
-    for my $f (@dates) {
-        $normed->{$f} = parse_date_abbrev( $normed->{$f} );
-    }
-
-    for my $k ( keys %$normed ) {
-        trim( $normed->{$k} );
-    }
-
-    return $normed;
 }
 
 my $a_file = shift or die $usage;
@@ -100,11 +107,9 @@ for my $c (@$c_recs) {
 }
 
 # first debug
-my ( $ref_id, $rec ) = each %recs;
-
-dump $rec;
-
-exit;
+#my ( $ref_id, $rec ) = each %recs;
+#dump $rec;
+#exit;
 
 # NCANDS has children + reports
 # the equivalent here is A == report
@@ -145,21 +150,96 @@ exit;
 # c fields
 # REFER_ID,CL_ID,CAS_ID,ABS_TYPE
 
+sub get_role {
+    my $perp = shift;
+
+    # TODO can more than one be true?
+    return 'parent'    if $perp->{PARENT};
+    return 'caretaker' if $perp->{CARETAKER};
+    return 'guardian'  if $perp->{LEG_GUARD};
+    return 'household' if $perp->{HSHLD_MBR};
+    return '';
+}
+
+sub get_perps {
+    my $people = shift;
+    my $victim = shift;
+    my @perps;
+    my %uniq;
+
+    #warn '=' x 80;
+    for my $p (@$people) {
+        if ( $p->{APRP} ) {
+
+            #warn "trying to match " . dump([$victim, $p]);
+            push @perps,
+                {
+                id   => $p->{CL_ID},
+                sex  => $p->{GENDER},
+                role => get_role($p),
+                r    => $p
+                };
+        }
+    }
+    return [ grep { !$uniq{ $_->{id} }++ } @perps ];
+}
+
+sub build_rows {
+    my $tree        = shift;
+    my $case        = $tree->{a}->[0];
+    my $people      = $tree->{b};
+    my $allegations = $tree->{c};
+    my @rows;
+    for my $p (@$people) {
+        if ( $p->{ALL_CHILD} and $p->{AVSC} ) {
+            my $r = {
+                ref_id           => $p->{REFER_ID},
+                ref_type         => $case->{REF_TYPE},
+                client_id        => $p->{CL_ID},
+                intake_date      => parse_date_abbrev( $case->{INTAKE_DT} ),
+                age              => $p->{AGE_AT_REFER},
+                sex              => $p->{GENDER},
+                race             => $p->{RACE_GROUP},
+                afcars_id        => $p->{STATE_MCI_ID},
+                case_id          => $case->{CAS_ID},
+                case_opened_date => parse_date_abbrev( $case->{CAS_OPN_DT} ),
+                case_closed_date =>
+                    parse_date_abbrev( $case->{LAST_CLOSE_DT} ),
+                service_accept_date =>
+                    parse_date_abbrev( $case->{SERVICE_ACCEPT_DT} ),
+                call_screen_code      => $case->{CALL_SCRN_CODE},
+                service_decision_code => $case->{SERVICE_DECISION_CODE},
+                allegation_code       => get_allegation_code(
+                    grep { $_->{CL_ID} eq $p->{CL_ID} } @$allegations
+                ),
+            };
+            remap( $r, "sex", \%sex );
+            my $perps = get_perps( $people, $p );
+            if ( scalar(@$perps) > $MAX_PERPS ) {
+                die "too many perps for case: " . dump($perps);
+            }
+            my $p_count = 1;
+            for my $perp (@$perps) {
+                for my $f (qw( id role sex )) {
+                    $r->{"perp${p_count}_${f}"} = $perp->{$f};
+                }
+                remap( $r, "perp${p_count}_sex", \%sex );
+                $p_count++;
+            }
+            push @rows, $r;
+        }
+    }
+    return [@rows];
+}
+
 my @csv_header = qw(
     ref_id
     client_id
     intake_date
-    child
-    alleged_perp
-    alleged_victim
-    parent
-    caretaker
-    legal_guardian
-    household_member
     age
-    gender
+    sex
     race
-    state_mci_id
+    afcars_id
     ref_type
     case_id
     case_opened_date
@@ -170,18 +250,27 @@ my @csv_header = qw(
     allegation_code
 );
 
+for my $c ( ( 1 .. $MAX_PERPS ) ) {
+    for my $f (qw( id role sex )) {
+        push @csv_header, "perp${c}_$f";
+    }
+}
+
 my $header   = [@csv_header];
 my $csv_file = $a_file;
-$csv_file =~ s,/.+?\.json,data-set-2-norm.csv,;
+$csv_file =~ s,/[^/]+\.json,/data-set-2-norm.csv,;
+print "Writing $csv_file\n";
 my $csv = Text::CSV_XS->new( { binary => 1, eol => $/, auto_diag => 1, } );
 $csv->column_names($header);
 open my $fh, ">:encoding(utf8)", $csv_file or die "$csv_file: $!";
 $csv->print( $fh, $header );
 
 for my $ref_id ( keys %recs ) {
-    my $r = norm_rec( $recs{$ref_id} );
-    $csv->print_hr( $fh, $r );
-    $fh->flush;
+    my $rows = build_rows( $recs{$ref_id} );
+    for my $r (@$rows) {
+        $csv->print_hr( $fh, $r );
+        $fh->flush;
+    }
 }
 
 close $fh;
